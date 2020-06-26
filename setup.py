4492edad800b27fde041ded8036155f6c9247107
Giulio Rossetti <giulio.rossetti@gmail.com>
2020-06-26 14:34:58 +0200

    :arrow_up: refactoring

--- setup.py
+++ setup.py
@@ -45,7 +45,6 @@ setup(name='nf1',
           # Specify the Python versions you support here. In particular, ensure
           # that you indicate whether you support Python 2, Python 3 or both.
           'Programming Language :: Python',
-          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3'
       ],
       keywords='community-discovery evaluation complex-networks',
